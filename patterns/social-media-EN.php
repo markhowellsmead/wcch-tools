094b9b6097dc88bcfa83be1db0b273dd1ef575bf
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-02-03 18:57:10 +0100

    Block patterns

---
+++ patterns/social-media-EN.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Title: Social media image English
+ * Slug: sht/social-media-EN
+ * Categories: featured
+ * Block Types: ["core/cover", "core/image"]
+ */
+?>
+
+<!-- wp:cover {"dimRatio":80,"gradient":"diagonal-primary","isDark":false,"align":"wide","className":"is-style-some-1080"} -->
+<div class="wp-block-cover alignwide is-light is-style-some-1080"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-80 has-background-dim has-background-gradient has-diagonal-primary-gradient-background"></span>
+	<div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"15rem"}},"textColor":"white","layout":{"type":"constrained"}} -->
+		<div class="wp-block-group has-white-color has-text-color"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"top":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--80)"><!-- wp:image {"align":"center","id":354,"width":120,"height":120,"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image aligncenter size-full is-resized"><img src="/wp-content/uploads/2023/02/WPCH-logo.png" alt="" class="wp-image-354" width="120" height="120" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+				<p class="has-text-align-center has-large-font-size"><strong>WordCamp Switzerland 2023</strong></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"textColor":"white","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-white-color has-text-color"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"70px"}}} -->
+				<h1 class="has-text-align-center" style="font-size:70px">Hello world</h1>
+				<!-- /wp:heading -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+				<p class="has-text-align-center has-large-font-size"><strong>switzerland.wordcamp.org<br>#WCCH</strong></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+</div>
+<!-- /wp:cover -->
